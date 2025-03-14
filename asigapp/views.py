510811d459fa86be5c9218b6fe6f42dd8e53edfb
Rew321 <rew23.dev@gmail.com>
2025-03-14 09:47:34 +0300

    second commit

--- asigapp/views.py
+++ asigapp/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>Home</h1>")
+    return HttpResponse("<h1>Let's Learn Django Signals</h1>")
